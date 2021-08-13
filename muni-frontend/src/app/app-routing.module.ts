9c676fe157f70c014ee89bb3b7d822dabd6e369b
juanvaleriomayta <jvaleriom@vallegrande.edu.pe>
2021-08-13 01:55:39 -0500

    Inicio del proyecto municipalidad

--- muni-frontend/src/app/app-routing.module.ts
+++ muni-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeFormularioComponent } from './pages/home/home-formulario/home-formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+
+  {
+    path: 'home',
+    component: HomeFormularioComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
